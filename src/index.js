503355b822b8117d3e7b3a33ebb385cc7f537c3a
Norman Bentley <43939955+normanbentley@users.noreply.github.com>
2021-10-30 12:06:07 +1300

    moving paddle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-let canvas = document.getElementById("gameScreen");
-
-let ctx = canvas.getContext("2d");
+import Paddle from "/src/paddle";
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
-ctx.clearRect(0, 0, 800, 600);
+
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
 
 var paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, 800, 600);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
--- src/paddle.js
+++ src/paddle.js
@@ -1,14 +1,21 @@
-class Paddle {
+export default class Paddle {
   constructor(gameWidth, gameHeight) {
     this.width = 150;
-    this.height = 30;
-    this.babboob = 5;
+    this.height = 20;
     this.position = {
       x: gameWidth / 2 - this.width / 2,
-      y: (gameHeight = this.height - 10)
+      y: gameHeight - this.height - 10
     };
   }
   draw(ctx) {
+    ctx.fillStyle = "hotpink";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  // how much time has changed since this has been updated?
+  // moves 5 frames a second
+  update(deltaTime) {
+    if (!deltaTime) return;
+    this.position.x += 5 / deltaTime;
   }
 }
