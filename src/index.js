6f0127ccc2643e39d8d75400ec96ed8740f6bb17
Norman Bentley <43939955+normanbentley@users.noreply.github.com>
2021-10-30 11:45:03 +1300

    paddle class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById("gameScreen");
 
 let ctx = canvas.getContext("2d");
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+ctx.clearRect(0, 0, 800, 600);
 
-ctx.fillStyle = "#f00";
-ctx.fillRect(20, 20, 100, 100);
+var paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
