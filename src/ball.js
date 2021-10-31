676d071d942e6b6edb7ee99f25f061dc9b1e058a
Norman Bentley <43939955+normanbentley@users.noreply.github.com>
2021-10-31 20:30:13 +1300

    initial commit

---
+++ src/ball.js
@@ -0,0 +1,33 @@
+export default class Ball {
+  constructor(game) {
+    this.gameWidth = game.gameWidth;
+    this.gameHeight = game.gameHeight;
+
+    this.image = document.getElementById("img_ball");
+    this.velocity = 5;
+    this.speed = { x: this.velocity, y: this.velocity };
+    this.position = { x: 10, y: 10 };
+    this.size = 16;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.size,
+      this.size
+    );
+  }
+  update(deltaTime) {
+    this.position.x += this.speed.x;
+    this.position.y += this.speed.y;
+
+    if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+      this.speed.x = -this.speed.x;
+    }
+    if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
+      this.speed.y = -this.speed.y;
+    }
+  }
+}
